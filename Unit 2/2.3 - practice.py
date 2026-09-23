449c2fa70416dd9fd7887562783ef65d8f2ea56a
EdassaGemechu <edassagmechu@gmail.com>
2026-09-23 13:23:33 -0500

    animal survey

---
+++ Unit 2/2.3 - practice.py
@@ -0,0 +1,5 @@
+fav_color = input("what is your favorite color?")
+
+print(fav_color)
+
+print("your Favorite color is"+ fav_color)
---
+++ Unit 2/AnimalSurvey.py
@@ -0,0 +1,8 @@
+Favorite_Animal =  input("What is your favorite animal?\n> ")
+Why_animal = input("Why is this your favorite animal?\n> ")
+Habitat = input("Where do they live?\n> ") 
+Fun_Fact = input("One Fun Fact about them\n> ")
+print("-------------------------------------------------------------")
+print("Your favorite animal is " + Favorite_Animal + " , it is your favorite because " + Why_animal + ", they live in " + Habitat + ", one fun fact about them is " + Fun_Fact)
+
+
